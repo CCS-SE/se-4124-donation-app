45448705d16f85d8e876078dc7c44c88bc88c539
karmslajo <karms.lajo@gmail.com>
2023-11-04 16:29:43 +0800

    add createFundraiser function

--- web3/contracts/DonationContract.sol
+++ web3/contracts/DonationContract.sol
@@ -18,6 +18,45 @@ contract DonationContract {
     }
 
     mapping(uint256 => Fundraiser) public fundraisers;
-
+    
     uint256 public numOfFundraisers = 0;
+
+    event FundraiserCreated(
+        uint256 fundraiserId,
+        address beneficiary,
+        string title,
+        string description,
+        string img,
+        uint256 targetAmount
+    );
+
+    function createFundraiser(
+        address _beneficiary,
+        string memory _title,
+        string memory _description,
+        string memory _img,
+        uint256 _targetAmount
+    ) public returns (uint256) {
+        Fundraiser storage fundraiser = fundraisers[numOfFundraisers];
+
+        fundraiser.beneficiary = _beneficiary;
+        fundraiser.title = _title;
+        fundraiser.description = _description;
+        fundraiser.img = _img;
+        fundraiser.targetAmount = _targetAmount;
+        fundraiser.amountCollected = 0;
+
+        numOfFundraisers++;
+
+        emit FundraiserCreated(
+            numOfFundraisers - 1,
+            _beneficiary,
+            _title,
+            _description,
+            _img,
+            _targetAmount
+        );
+
+        return numOfFundraisers - 1;
+    }
 }
