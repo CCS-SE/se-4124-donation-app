27c4d20f047e097993c0612f7d604d4780f7a6e8
karmslajo <karms.lajo@gmail.com>
2023-11-09 13:47:31 +0800

    display claim button for beneficiary's fundraiser

--- web-react/components/Card/FundraiserCard.tsx
+++ web-react/components/Card/FundraiserCard.tsx
@@ -8,12 +8,13 @@ import {
 } from '@nextui-org/react';
 import AddDonationModal from '../Modal/AddDonationModal';
 import { FaEthereum } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+import { useAddress } from '@thirdweb-dev/react';
 
 interface FundraiserCardProps {
   fundraiserId: number;
   beneficiary: string;
   title: string;
-  description?: string;
   img: string;
   targetAmount: string;
   amountCollected: string;
@@ -23,16 +24,21 @@ export default function FundraiserCard({
   fundraiserId,
   beneficiary,
   title,
-  description,
   img,
   targetAmount,
   amountCollected,
 }: FundraiserCardProps) {
+  const address = useAddress();
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { push } = useRouter();
 
   return (
     <>
-      <Card shadow='sm'>
+      <Card
+        shadow='sm'
+        onPress={() => push(`fundraiser/${fundraiserId}`)}
+        isPressable
+      >
         <CardBody className='overflow-visible p-0'>
           <Image
             shadow='sm'
@@ -44,7 +50,7 @@ export default function FundraiserCard({
           />
         </CardBody>
         <CardFooter>
-          <div className='flex-1 flex-col '>
+          <div className='flex-1 flex-col'>
             <b className='text-blue-500 text-xl'>{title}</b>
             <div className='flex flex-row justify-between items-center '>
               <div className='flex flex-row'>
@@ -53,16 +59,30 @@ export default function FundraiserCard({
                 <p className=' text-blue-700'>{targetAmount}</p>
               </div>
               <div>
-                <Button size='md' radius='lg' color='primary' onPress={onOpen}>
-                  Donate
-                </Button>
+                {beneficiary !== address ? (
+                  <Button
+                    size='md'
+                    radius='lg'
+                    color='primary'
+                    onPress={onOpen}
+                  >
+                    Donate
+                  </Button>
+                ) : (
+                  <Button
+                    size='md'
+                    radius='lg'
+                    color='success'
+                    className='text-white'
+                  >
+                    Claim
+                  </Button>
+                )}
               </div>
             </div>
             <div className='flex flex-row bg-violet-200 mt-2 rounded-xl p-1 pl-2'>
-              <FaEthereum color='blueviolet'/>
-              <p className='text-[11px] text-default-500 ml-2'>
-                {beneficiary}
-              </p>
+              <FaEthereum color='blueviolet' />
+              <p className='text-[11px] text-default-500 ml-2'>{beneficiary}</p>
             </div>
           </div>
         </CardFooter>
